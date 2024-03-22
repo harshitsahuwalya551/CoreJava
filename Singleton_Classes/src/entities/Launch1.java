32bf5d6582e26ca775bf716cd23b002012efeb71
Harshit Kumar <kumarharshit551@gmail.com>
2024-03-22 16:28:20 +0530

    Singleton Classes

---
+++ Singleton_Classes/src/entities/Launch1.java
@@ -0,0 +1,9 @@
+package entities;
+
+public class Launch1 {
+	public static void main(String[] args) {
+		Runtime r1 = Runtime.getRuntime();
+		Runtime r2 = Runtime.getRuntime();
+		System.out.println(r1==r2);
+	}
+}
---
+++ Singleton_Classes/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Harshit Kumar
+ *
+ */
+module Singleton_Classes {
+}
